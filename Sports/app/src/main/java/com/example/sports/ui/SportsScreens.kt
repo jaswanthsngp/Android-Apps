1b7b62e9ca758c11bac4ca52b1631f6ce1bdf9e5
jaswanthsngp <sanagapallijaswanth@gmail.com>
2023-11-13 13:01:44 +0530

    Adaptive Layout Practice

---
+++ Sports/app/src/main/java/com/example/sports/ui/SportsScreens.kt
@@ -0,0 +1,502 @@
+/*
+ * Copyright (c) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.sports.ui
+
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.pluralStringResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.sports.R
+import com.example.sports.data.LocalSportsDataProvider
+import com.example.sports.model.Sport
+import com.example.sports.ui.theme.SportsTheme
+
+/**
+ * Main composable that serves as container
+ * which displays content according to [uiState] and [windowSize]
+ */
+@Composable
+fun SportsApp(
+    windowSize: WindowWidthSizeClass
+) {
+    val viewModel: SportsViewModel = viewModel()
+    val uiState by viewModel.uiState.collectAsState()
+
+    when(windowSize) {
+        WindowWidthSizeClass.Compact -> {
+            CompactLayout(
+                viewModel = viewModel,
+                uiState = uiState
+            )
+        }
+        WindowWidthSizeClass.Medium -> {
+            WideLayout(
+                viewModel = viewModel,
+                uiState = uiState,
+                listWidth = 0.5f
+            )
+        }
+        WindowWidthSizeClass.Expanded -> {
+            WideLayout(
+                viewModel = viewModel,
+                uiState = uiState,
+                listWidth = 0.33f
+            )
+        }
+        else -> {
+            CompactLayout(
+                viewModel = viewModel,
+                uiState = uiState
+            )
+        }
+    }
+
+}
+
+
+/**
+* Composable that displays app on Compact Screens
+*/
+@Composable
+fun CompactLayout (
+    viewModel: SportsViewModel,
+    uiState: SportsUiState
+) {
+    Scaffold(
+        topBar = {
+            SportsAppBar(
+                isShowingListPage = uiState.isShowingListPage,
+                onBackButtonClick = { viewModel.navigateToListPage() },
+            )
+        }
+    ) { innerPadding ->
+        if (uiState.isShowingListPage) {
+            SportsList(
+                sports = uiState.sportsList,
+                onClick = {
+                    viewModel.updateCurrentSport(it)
+                    viewModel.navigateToDetailPage()
+                },
+                contentPadding = innerPadding,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        top = dimensionResource(R.dimen.padding_medium),
+                        start = dimensionResource(R.dimen.padding_medium),
+                        end = dimensionResource(R.dimen.padding_medium),
+                    )
+            )
+        } else {
+            SportsDetail(
+                selectedSport = uiState.currentSport,
+                contentPadding = innerPadding,
+                onBackPressed = {
+                    viewModel.navigateToListPage()
+                }
+            )
+        }
+    }
+}
+
+
+/**
+* Composable that displays app on Medium and Expanded Layouts
+*/
+@Composable
+fun WideLayout(
+    viewModel: SportsViewModel,
+    uiState: SportsUiState,
+    listWidth: Float
+) {
+    Scaffold(
+        topBar = {
+            SportsAppBar(
+                isShowingListPage = true,
+                onBackButtonClick = {  },
+            )
+        }
+    ) { innerPadding ->
+        Row (
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            SportsList(
+                sports = uiState.sportsList,
+                onClick = {
+                    viewModel.updateCurrentSport(it)
+                    viewModel.navigateToDetailPage()
+                },
+                contentPadding = innerPadding,
+                modifier = Modifier
+                    .fillMaxWidth(listWidth)
+                    .padding(
+                        top = dimensionResource(R.dimen.padding_medium),
+                        start = dimensionResource(R.dimen.padding_medium),
+                        end = dimensionResource(R.dimen.padding_medium),
+                    )
+            )
+            SportsDetail(
+                selectedSport = uiState.currentSport,
+                contentPadding = innerPadding,
+                onBackPressed = {
+                    viewModel.navigateToListPage()
+                },
+            )
+        }
+    }
+}
+
+
+
+/**
+ * Composable that displays the topBar and displays back button if back navigation is possible.
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SportsAppBar(
+    onBackButtonClick: () -> Unit,
+    isShowingListPage: Boolean,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text =
+                if (!isShowingListPage) {
+                    stringResource(R.string.detail_fragment_label)
+                } else {
+                    stringResource(R.string.list_fragment_label)
+                }
+            )
+        },
+        navigationIcon = if (!isShowingListPage) {
+            {
+                IconButton(onClick = onBackButtonClick) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back_button)
+                    )
+                }
+            }
+        } else {
+            { Box {} }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        ),
+        modifier = modifier,
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun SportsListItem(
+    sport: Sport,
+    onItemClick: (Sport) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        elevation = CardDefaults.cardElevation(),
+        modifier = modifier,
+        shape = RoundedCornerShape(dimensionResource(R.dimen.card_corner_radius)),
+        onClick = { onItemClick(sport) }
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .size(dimensionResource(R.dimen.card_image_height))
+        ) {
+            SportsListImageItem(
+                sport = sport,
+                modifier = Modifier.size(dimensionResource(R.dimen.card_image_height))
+            )
+            Column(
+                modifier = Modifier
+                    .padding(
+                        vertical = dimensionResource(R.dimen.padding_small),
+                        horizontal = dimensionResource(R.dimen.padding_medium)
+                    )
+                    .weight(1f)
+            ) {
+                Text(
+                    text = stringResource(sport.titleResourceId),
+                    style = MaterialTheme.typography.titleMedium,
+                    modifier = Modifier.padding(bottom = dimensionResource(R.dimen.card_text_vertical_space))
+                )
+                Text(
+                    text = stringResource(sport.subtitleResourceId),
+                    style = MaterialTheme.typography.bodySmall,
+                    color = MaterialTheme.colorScheme.secondary,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 3
+                )
+                Spacer(Modifier.weight(1f))
+                Row {
+                    Text(
+                        text = pluralStringResource(
+                            R.plurals.player_count_caption,
+                            sport.playerCount,
+                            sport.playerCount
+                        ),
+                        style = MaterialTheme.typography.bodySmall
+                    )
+                    Spacer(Modifier.weight(1f))
+                    if (sport.olympic) {
+                        Text(
+                            text = stringResource(R.string.olympic_caption),
+                            style = MaterialTheme.typography.labelMedium
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun SportsListImageItem(sport: Sport, modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(sport.imageResourceId),
+            contentDescription = null,
+            alignment = Alignment.Center,
+            contentScale = ContentScale.FillWidth
+        )
+    }
+}
+
+@Composable
+private fun SportsList(
+    sports: List<Sport>,
+    onClick: (Sport) -> Unit,
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(0.dp),
+) {
+    LazyColumn(
+        contentPadding = contentPadding,
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium)),
+        modifier = modifier,
+    ) {
+        items(sports, key = { sport -> sport.id }) { sport ->
+            SportsListItem(
+                sport = sport,
+                onItemClick = onClick
+            )
+        }
+    }
+}
+
+@Composable
+private fun SportsDetail(
+    selectedSport: Sport,
+    onBackPressed: () -> Unit,
+    contentPadding: PaddingValues,
+    modifier: Modifier = Modifier
+) {
+    BackHandler {
+        onBackPressed()
+    }
+    val scrollState = rememberScrollState()
+    val layoutDirection = LocalLayoutDirection.current
+    Box(
+        modifier = modifier
+            .verticalScroll(state = scrollState)
+            .padding(top = contentPadding.calculateTopPadding())
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(
+                    bottom = contentPadding.calculateTopPadding(),
+                    start = contentPadding.calculateStartPadding(layoutDirection),
+                    end = contentPadding.calculateEndPadding(layoutDirection)
+                )
+        ) {
+            Box {
+                Box {
+                    Image(
+                        painter = painterResource(selectedSport.sportsImageBanner),
+                        contentDescription = null,
+                        alignment = Alignment.TopCenter,
+                        contentScale = ContentScale.FillWidth,
+                    )
+                }
+                Column(
+                    Modifier
+                        .align(Alignment.BottomStart)
+                        .fillMaxWidth()
+                        .background(
+                            Brush.verticalGradient(
+                                listOf(Color.Transparent, MaterialTheme.colorScheme.scrim),
+                                0f,
+                                400f
+                            )
+                        )
+                ) {
+                    Text(
+                        text = stringResource(selectedSport.titleResourceId),
+                        style = MaterialTheme.typography.headlineLarge,
+                        color = MaterialTheme.colorScheme.inverseOnSurface,
+                        modifier = Modifier
+                            .padding(horizontal = dimensionResource(R.dimen.padding_small))
+                    )
+                    Row(
+                        modifier = Modifier.padding(dimensionResource(R.dimen.padding_small))
+                    ) {
+                        Text(
+                            text = pluralStringResource(
+                                R.plurals.player_count_caption,
+                                selectedSport.playerCount,
+                                selectedSport.playerCount
+                            ),
+                            style = MaterialTheme.typography.bodySmall,
+                            color = MaterialTheme.colorScheme.inverseOnSurface,
+                        )
+                        Spacer(Modifier.weight(1f))
+                        Text(
+                            text = stringResource(R.string.olympic_caption),
+                            style = MaterialTheme.typography.labelMedium,
+                            color = MaterialTheme.colorScheme.inverseOnSurface,
+                        )
+                    }
+                }
+            }
+            Text(
+                text = stringResource(selectedSport.sportDetails),
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(
+                    vertical = dimensionResource(R.dimen.padding_detail_content_vertical),
+                    horizontal = dimensionResource(R.dimen.padding_detail_content_horizontal)
+                )
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun SportsListItemPreview() {
+    SportsTheme {
+        SportsListItem(
+            sport = LocalSportsDataProvider.defaultSport,
+            onItemClick = {}
+        )
+    }
+}
+
+@Preview
+@Composable
+fun SportsListPreview() {
+    SportsTheme {
+        Surface {
+            SportsList(
+                sports = LocalSportsDataProvider.getSportsData(),
+                onClick = {},
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun SportDetailPreview() {
+    SportsTheme {
+        Surface {
+            SportsDetail(
+                selectedSport = LocalSportsDataProvider.defaultSport,
+                onBackPressed = {  },
+                contentPadding = PaddingValues(4.dp)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CompactPreview() {
+    SportsTheme {
+        Surface {
+            SportsApp(windowSize = WindowWidthSizeClass.Compact)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MediumPreview() {
+    SportsTheme {
+        Surface {
+            SportsApp(windowSize = WindowWidthSizeClass.Medium)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ExpandedPreview() {
+    SportsTheme {
+        Surface {
+            SportsApp(windowSize = WindowWidthSizeClass.Expanded)
+        }
+    }
+}
